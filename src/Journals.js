b875a2dd4a21f3372f091dc522cec765b02a03f1
CLQuantizer <ezioxxk@gmail.com>
2022-10-16 19:19:22 +0100

    add logout and user greeting

--- src/Journals.js
+++ src/Journals.js
@@ -1,53 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Box,
-  Text,
-  VStack,
-  Grid,
-  ListItem,
-  UnorderedList,
+  Box, Text, VStack, Grid, ListItem, UnorderedList,
 } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
 import Config from './Config';
 
-const journalUrl = Config.api+'users/read/alljournals/';
+const journalUrl = Config.api + 'users/read/alljournals/';
 
-function Journals(){
-	document.title='Journals';
-	const [ListOfJournals,setListOfJournal] = useState([]);
-   	
-	async function getData(){
-		try{
-			const response = await fetch(journalUrl,{method:'GET'});
-			const json = await response.json();
+function Journals() {
+  document.title = 'Journals';
+  const [ListOfJournals, setListOfJournals] = useState([]);
+  const results = ListOfJournals.map((j) =>
+    <ListItem p='2' mt='15' key={j['title']}>
+      <Text fontSize='2xl' fontWeight='bold'>{j['title']}</Text>
+      <Text fontSize='m' fontWeight='bold' color='#285E61'>By {j['author']} on: {j['date']}</Text>
+      <ReactMarkdown>{j['body']}</ReactMarkdown>
+    </ListItem>
+  );
+  // const [ListOfJournals, setListOfJournal] = useState([]);
+  // setListOfJournal(getData);
+  useEffect(() => {
+    fetch(journalUrl, { method: 'GET' })
+      .then(response => {
+        if (response.ok) {
+          console.log('the response for from API is:');
+          console.log(response.body);
+          return response.json();
+        }
+        throw response;
+      })
+      .then(data=>{
+        setListOfJournals(data);
+      })
+      .catch(error=>{
+        console.log("error: "+error);
+      });
+  }, []);
 
-			console.log('the response for from API is:');
-						console.log(json);
-			    	setListOfJournal(json);
-			}catch(error){console.error(error);}
-		}
 
-	useEffect(() => {
-		getData();
- 	 }, []);
-	
-	return (
-      <Box maxW='lg' textAlign="left">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-          	<UnorderedList fontSize='sm'>
-          		{ListOfJournals.map((j)=>
-          			<ListItem p='2' mt='15' key={j['title']}>
-          					<Text fontSize='2xl' fontWeight='bold' >{j['title']}</Text>
-							<Text fontSize='m' fontWeight='bold' color='#285E61'>By {j['author']} on: {j['date']}</Text>
-							<ReactMarkdown>{j['body']}</ReactMarkdown>
-          			</ListItem>)}
-  			</UnorderedList>
-	        </VStack>
-        </Grid>
-      </Box>
-		
-	);	
+  return (<Box maxW='lg' textAlign='left'>
+      <Grid minH='100vh' p={3}>
+        <VStack spacing={8}>
+          <UnorderedList fontSize='sm'>
+            {results}
+          </UnorderedList>
+        </VStack>
+      </Grid>
+    </Box>
+
+  );
 }
 
 export default Journals;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { 
+import {
   ChakraProvider,
   Button,
   Box,
@@ -7,99 +7,111 @@ import {
   GridItem,
   theme,
 } from '@chakra-ui/react';
-import React, {StrictMode, useEffect, useState} from 'react';
+import React, { StrictMode, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import MyPage from './MyPage';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import Journals from './Journals'
+import Journals from './Journals';
 import Config from './Config';
 
 const serverUrl = Config.server;
 
-function Index(){
-      const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
-      const [expiration, setExpiration] = useState(0);
-      const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('accessToken') !== null);
+function Index() {
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
+  const [expiration, setExpiration] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('accessToken') !== null);
+  const [user, setUser] = useState(localStorage.getItem("user"));
 
-    const readToken = async(isLoggedIn)=>{
-      if(isLoggedIn){return;}
-      const token = localStorage.getItem('accessToken');
-      const exp = localStorage.getItem('expiration');
-      if(token && exp){
-        if(exp >Math.round(new Date().getTime()/1000)){
-          console.log('token is valid');
-          setAccessToken(localStorage.getItem('accessToken'));
-          setExpiration(localStorage.getItem('expiration'));
-          setIsLoggedIn(true);
-        }else{
-          setAccessToken('');
-          setExpiration(0);
-          setIsLoggedIn(false);
-        }
-      }else{
+  const readToken = async (isLoggedIn) => {
+    if (isLoggedIn) {
+      return;
+    }
+    const token = localStorage.getItem('accessToken');
+    const exp = localStorage.getItem('expiration');
+    if (token && exp) {
+      if (exp > Math.round(new Date().getTime() / 1000)) {
+        console.log('token is valid');
+        setAccessToken(localStorage.getItem('accessToken'));
+        setExpiration(parseInt(localStorage.getItem('expiration')));
+        setIsLoggedIn(true);
+        setUser(localStorage.getItem("user"));
+      } else {
         setAccessToken('');
         setExpiration(0);
         setIsLoggedIn(false);
+        setUser("there");
       }
+    } else {
+      setAccessToken('');
+      setExpiration(0);
+      setIsLoggedIn(false);
+      setUser("there");
     }
-
-    useEffect(()=>{
-      if(accessToken!==''){
-        readToken(isLoggedIn).then(()=>{console.log('now I am logged in right?: '+isLoggedIn);});}
-    },[isLoggedIn]);
+  };
+  const getUser = ()=>{
+    return user.toString();
+  }
+  useEffect(() => {
+    if (accessToken !== '') {
+      readToken(isLoggedIn).then(() => {
+        console.log('now I am logged in right?: ' + isLoggedIn);
+      });
+    }
+  }, [accessToken, isLoggedIn]);
 
   return (
-  <StrictMode>
-  <ChakraProvider theme={theme}>
-  <Box textAlign="center" fontSize="xl">
-    <Grid minH="5vh" p={3}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-    </Grid>
+    <StrictMode>
+      <ChakraProvider theme={theme}>
+        <Box textAlign='center' fontSize='xl'>
+          <Grid minH='5vh' p={3}>
+            <ColorModeSwitcher justifySelf='flex-end' />
+          </Grid>
 
-    <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-      <GridItem w='100%' h='10' >
-        <Button variant = 'outline'>
-          <Link href ={serverUrl}>Home</Link>
-        </Button>
-      </GridItem>
+          <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+            <GridItem w='100%' h='10'>
+              <Button variant='outline'>
+                <Link href={serverUrl}>Home</Link>
+              </Button>
+            </GridItem>
 
-      <GridItem w='100%' h='10'>
-        <Button variant = 'outline'>
-          <Link href = {serverUrl +'journals/'}>Journals</Link>
-        </Button>
-      </GridItem>
+            <GridItem w='100%' h='10'>
+              <Button variant='outline'>
+                <Link href={serverUrl + 'journals/'}>Journals</Link>
+              </Button>
+            </GridItem>
 
-      <GridItem w='100%' h='10'>
-        <Button variant = 'outline'>
-          {/* <Link href ={serverUrl+'login/'}>{isLoggedIn===true ? 'Login':'logout' }</Link> */}
-          <Link href ={serverUrl+'login/'}>Login</Link>
-        </Button>
-      </GridItem>
+            <GridItem w='100%' h='10'>
+              <Button variant='outline'>
+                {/* <Link href ={serverUrl+'login/'}>{isLoggedIn===true ? 'Login':'logout' }</Link> */}
+                <Link href={serverUrl + 'login/'}>{isLoggedIn ? 'Hi '+getUser(): 'login'}</Link>
+              </Button>
+            </GridItem>
 
-    </Grid>
-  </Box>
-  <br/>
-    <BrowserRouter>
-    <Routes>
-      <Route path = "" element={<App/>}/>
-      <Route path = "journals/" element={<Journals/>}/>
-      <Route path = "login/" element={<MyPage 
-          accessToken={accessToken}
-          isLoggedIn={isLoggedIn}
-          expiration={expiration}
-          setAccessToken={setAccessToken} 
-          setIsLoggedIn={setIsLoggedIn}
-          setExpiration={setExpiration}
-        />}
-      />
-    </Routes>
-    </BrowserRouter>
-  </ChakraProvider>  
-</StrictMode>);
+          </Grid>
+        </Box>
+        <br />
+        <BrowserRouter>
+          <Routes>
+            <Route path='' element={<App />} />
+            <Route path='journals/' element={<Journals />} />
+            <Route path='login/' element={<MyPage
+              accessToken={accessToken}
+              isLoggedIn={isLoggedIn}
+              expiration={expiration}
+              setAccessToken={setAccessToken}
+              setIsLoggedIn={setIsLoggedIn}
+              setExpiration={setExpiration}
+              setUser={setUser}
+            />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </StrictMode>);
 }
 
-ReactDOM.render(<Index/>, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
 
