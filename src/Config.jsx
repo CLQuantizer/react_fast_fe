5d25c549001add9a884369ade7332dfca1ddf802
CLQuantizer <ezioxxk@gmail.com>
2022-11-08 02:26:37 +0000

    fix config

--- src/Config.jsx
+++ src/Config.jsx
@@ -10,11 +10,11 @@ class Config {
       this.frontEndKeyWord = 'npm';
       this.publicUrl = "";
     } else if (this.mode === 'production') {
-      this.server = 'http://localhost:3000/';
-      this.api = 'http://localhost:8000/';
-      this.glove = 'http://localhost:8001/';
-      this.infra = 'http://localhost:8002/';
-      this.dataPanel = 'http://localhost:8003/';
+      this.server = 'https://localhost:3000/';
+      this.api = "https://139.162.225.136/api/journal/"
+      this.glove = "https://139.162.225.136/api/word/"
+      this.infra = "https://139.162.225.136/api/infra/"
+      this.dataPanel = "https://139.162.225.136/api/datapanel/"
       this.frontEndKeyWord = 'pm2';
       this.publicUrl = "https://139.162.225.136/";
     } else {
