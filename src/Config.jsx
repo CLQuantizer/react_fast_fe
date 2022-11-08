0481110c95b41d4d06973cb1efe419d744e2e5d6
CLQuantizer <ezioxxk@gmail.com>
2022-11-08 02:01:43 +0000

    update prod config

--- src/Config.jsx
+++ src/Config.jsx
@@ -9,11 +9,11 @@ class Config {
       this.dataPanel = 'http://localhost:8003/';
       this.frontEndKeyWord = 'npm';
     } else if (this.mode === 'production') {
-      this.server = 'http://langedev.net/';
-      this.api = 'http://langedev.net:8000/';
-      this.glove = 'http://langedev.net:8001/';
-      this.infra = 'http://langedev.net:8002/';
-      this.dataPanel = 'http://langedev.net:8003/';
+      this.server = 'http://localhost:3000/';
+      this.api = 'http://localhost:8000/';
+      this.glove = 'http://localhost:8001/';
+      this.infra = 'http://localhost:8002/';
+      this.dataPanel = 'http://localhost:8003/';
       this.frontEndKeyWord = 'pm2';
     } else {
       throw new Error('mode must be either \'development\' or \'production\'');
